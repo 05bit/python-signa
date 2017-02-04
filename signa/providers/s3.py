ae73746d3a71e9da973ed984204d7d80cfae5949
Alexey Kinev <rudy@05bit.com>
2017-02-04 17:29:29 +0300

    Fix: properly handle US East region for S3

--- signa/providers/s3.py
+++ signa/providers/s3.py
@@ -4,12 +4,19 @@ from .aws import sign_aws
 def new(method=None, region=None, bucket=None, key=None,
         auth=None, headers=None, payload=None):
     headers = headers.copy() if headers else {}
-    # headers['host'] = '%s.s3.%s.amazonaws.com' % (bucket, region)
-    headers['host'] = 's3-%s.amazonaws.com' % region
+
+    # Details on buckets URLs:
+    # http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html
+    if region and (region != 'us-east-1'):
+        headers['host'] = 's3-%s.amazonaws.com' % region
+    else:
+        headers['host'] = 's3.amazonaws.com'
+
     if key:
         uri = '/%s/%s' % (bucket, key)
     else:
         uri = '/%s' % bucket
+
     headers.update(sign_aws(
         method=method,
         region=region,
@@ -18,6 +25,7 @@ def new(method=None, region=None, bucket=None, key=None,
         auth=auth,
         headers=headers,
         payload=payload))
+
     return {
         'url': 'https://%s%s' % (headers['host'], uri),
         'headers': headers,
