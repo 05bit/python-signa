121735e270685a96c40d19cd05a560795dce632a
Alexey Kinev <rudy@05bit.com>
2020-11-13 01:04:14 +0300

    Use region-only hostname format for B2

--- signa/providers/b2.py
+++ signa/providers/b2.py
@@ -11,7 +11,15 @@ def new(method=None, region=None, bucket=None, key=None,
 
     assert region in REGIONS
 
-    headers['host'] = '%s.s3.%s.backblazeb2.com' % (bucket, region)
+    # headers['host'] = '%s.s3.%s.backblazeb2.com' % (bucket, region)
+
+    # https://s3.<region>.backblazeb2.com/<bucket>
+    headers['host'] = 's3.%s.backblazeb2.com' % region
+
+    if key:
+        rel_uri = '/%s/%s' % (bucket, key)
+    else:
+        rel_uri = '/%s' % bucket
 
     rel_uri = ('/%s' % key) if key else '/'
 
